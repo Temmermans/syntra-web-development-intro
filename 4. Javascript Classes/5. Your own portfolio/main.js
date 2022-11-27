55a29e6f03fd71c2ba141c843a0da66e732bafc6
Simon Temmerman <simon.temmerman@dscribe.pro>
2022-11-27 17:06:14 +0100

    add portfolio example

---
+++ 4. Javascript Classes/5. Your own portfolio/main.js
@@ -0,0 +1,47 @@
+(function () {
+  const doc = document;
+  const body = doc.body;
+  const lightSwitch = doc.getElementById("lights-toggle");
+  /* global ScrollReveal */
+  const sr = (window.sr = ScrollReveal());
+
+  window.addEventListener("load", function () {
+    body.classList.add("is-loaded");
+  });
+
+  // Reveal animations
+  function revealAnimations() {
+    sr.reveal(".feature", {
+      duration: 600,
+      distance: "20px",
+      easing: "cubic-bezier(0.215, 0.61, 0.355, 1)",
+      origin: "right",
+      viewFactor: 0.2,
+    });
+  }
+
+  if (body.classList.contains("has-animations")) {
+    window.addEventListener("load", revealAnimations);
+  }
+
+  // Light switcher
+  if (lightSwitch) {
+    window.addEventListener("load", checkLights);
+    lightSwitch.addEventListener("change", checkLights);
+  }
+
+  function checkLights() {
+    let labelText = lightSwitch.parentNode.querySelector(".label-text");
+    if (lightSwitch.checked) {
+      body.classList.remove("lights-off");
+      if (labelText) {
+        labelText.innerHTML = "dark";
+      }
+    } else {
+      body.classList.add("lights-off");
+      if (labelText) {
+        labelText.innerHTML = "light";
+      }
+    }
+  }
+})();
